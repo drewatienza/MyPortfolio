ba02329ef7e8ac5a5d46d9a69a47dfa681e47725
drewatienza <atienza.drew@gmail.com>
2017-08-01 04:33:53 -0700

    Implemented gritter for notifications

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Drew Atienza Portfolio"
     @seo_keywords = "Drew Atienza portfolio"
   end
 end
